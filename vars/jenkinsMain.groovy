20a75d89fc31df061c22724c0d776bb980a032d9
Jose Rodriguez <jose0797@gmail.com>
2019-09-15 12:12:33 -0400

    Testing

--- vars/jenkinsMain.groovy
+++ vars/jenkinsMain.groovy
@@ -5,9 +5,9 @@ pipeline {
         stage('build') {
             steps {
                 script {
-                    echo env.JOB_NAME
-                    sh "mkdir -p $GOPATH/src/${env.JOB_NAME}"
-                    sh "ln -s $WORKSPACE $GOPATH/src/${env.JOB_NAME}"
+                    // echo env.JOB_NAME
+                    // sh "mkdir -p $GOPATH/src/${env.JOB_NAME}"
+                    // sh "ln -s $WORKSPACE $GOPATH/src/${env.JOB_NAME}"
                     sh 'go build -o subway'
                     stash "workspace"
                 }
