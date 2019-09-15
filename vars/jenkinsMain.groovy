aa7818adc566bb12839a2f292af47a65e3a22a50
Jose Rodriguez <jose0797@gmail.com>
2019-09-15 14:32:58 -0400

    Gopath

--- vars/jenkinsMain.groovy
+++ vars/jenkinsMain.groovy
@@ -18,9 +18,9 @@ pipeline {
                 script {
                     cleanWs()
                     unstash "workspace"
-                    // echo env.JOB_NAME
-                    // sh "mkdir -p $GOPATH/src/${env.JOB_NAME}"
-                    // sh "ln -s $WORKSPACE $GOPATH/src/${env.JOB_NAME}"
+                    echo env.JOB_NAME
+                    sh "mkdir -p $GOPATH/src/${env.JOB_NAME}"
+                    sh "ln -s $WORKSPACE $GOPATH/src/${env.JOB_NAME}"
                     sh 'go build -o subway'
                     stash "workspace"
                 }
