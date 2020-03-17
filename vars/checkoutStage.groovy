a5b5ee89eb848b59ebbd99b48597c1d2e1152c96
Jose Rodriguez <jose0797@gmail.com>
2020-03-17 14:11:18 -0400

    Testing

--- vars/checkoutStage.groovy
+++ vars/checkoutStage.groovy
@@ -3,7 +3,9 @@ import space.joserod.configs.Config
 def call(Config configs) {
     cleanWs()
     checkout scm 
+    def datas = readYaml file: 'configuration.yml'
     sh 'ls -la'
+    echo "Dta: ${datas}"
     error "Unstable, exiting now..."
     stash "workspace"
 }
