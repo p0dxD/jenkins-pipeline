5cf01638d25bc29044fbc96ed59f4411b0b20a80
Jose Rodriguez <jose0797@gmail.com>
2020-03-17 21:06:56 -0400

    Testing

--- vars/checkoutStage.groovy
+++ vars/checkoutStage.groovy
@@ -1,10 +1,19 @@
 import space.joserod.configs.Config
 
+// import java.util.LinkedHashMap
+// import java.util.Map
+
 def call(Config configs) {
     cleanWs()
     checkout scm 
-    def datas = readYaml file: 'configuration.yml'
+    LinkedHashMap datas = readYaml file: 'configuration.yml'
     echo ""+datas.getClass()
+
+    for (Map.Entry<String, ArrayList<String>> entry : datas.entrySet()) {
+        String key = entry.getKey();
+        ArrayList<String> value = entry.getValue();
+        // now work with key and value...
+    }
     sh 'ls -la'
     echo "Dta: ${datas}"
     error "Unstable, exiting now..."
