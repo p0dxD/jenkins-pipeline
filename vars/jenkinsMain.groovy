e9f4c698b842069256e04a0c3a3e0bda94b75934
Jose Rodriguez <jose0797@gmail.com>
2020-03-18 16:41:54 -0400

    Testing

--- vars/jenkinsMain.groovy
+++ vars/jenkinsMain.groovy
@@ -13,50 +13,50 @@ def call(){
                     }
                 }
             }
-            stage('Post Chechout') {
-                    agent { label "builder.ci.jenkins"}
-                steps {
-                    script {
-                        cleanWs()
-                        unstash "workspace"
-                        def resourceContent = libraryResource("scripts/post-checkout.sh")
-                        writeFile(file: "post-checkout.sh", text: resourceContent)
-                        sh "bash post-checkout.sh"
-                        stash "workspace"
-                    }
-                }
-            }
-            stage('build') {
-                    agent { label "builder.ci.jenkins"}
-                steps {
-                    script {
-                        cleanWs()
-                        unstash "workspace"
-                        error "Unstable, exiting now..."
-                        withEnv(["GOPATH=$WORKSPACE", "GOBIN=$GOPATH/bin"]) {
-                            sh "mkdir src bin && go get ./..."
-                            env.PATH="${env.GOPATH}/bin:$PATH"
-                            String repoName = scm.getUserRemoteConfigs()[0].getUrl().tokenize('/').last().split("\\.")[0]
-                            sh "go get -d ./pkg/..."
-                            sh "go install"
-                            sh 'go build -o subway main.go'
+            // stage('Post Chechout') {
+            //         agent { label "builder.ci.jenkins"}
+            //     steps {
+            //         script {
+            //             cleanWs()
+            //             unstash "workspace"
+            //             def resourceContent = libraryResource("scripts/post-checkout.sh")
+            //             writeFile(file: "post-checkout.sh", text: resourceContent)
+            //             sh "bash post-checkout.sh"
+            //             stash "workspace"
+            //         }
+            //     }
+            // }
+            // stage('build') {
+            //         agent { label "builder.ci.jenkins"}
+            //     steps {
+            //         script {
+            //             cleanWs()
+            //             unstash "workspace"
+            //             error "Unstable, exiting now..."
+            //             withEnv(["GOPATH=$WORKSPACE", "GOBIN=$GOPATH/bin"]) {
+            //                 sh "mkdir src bin && go get ./..."
+            //                 env.PATH="${env.GOPATH}/bin:$PATH"
+            //                 String repoName = scm.getUserRemoteConfigs()[0].getUrl().tokenize('/').last().split("\\.")[0]
+            //                 sh "go get -d ./pkg/..."
+            //                 sh "go install"
+            //                 sh 'go build -o subway main.go'
                         
-                            stash "workspace"
-                        }
-                    }
-                }
-            }
-            stage('Create image') {
-                    agent { label "builder.ci.jenkins"}
-                steps {
-                    script {
-                        cleanWs()
-                        unstash "workspace"
-                        sh "cat ./docker/dockerize.sh"
-                        sh './docker/dockerize.sh'
-                    }
-                }
-            }
+            //                 stash "workspace"
+            //             }
+            //         }
+            //     }
+            // }
+            // stage('Create image') {
+            //         agent { label "builder.ci.jenkins"}
+            //     steps {
+            //         script {
+            //             cleanWs()
+            //             unstash "workspace"
+            //             sh "cat ./docker/dockerize.sh"
+            //             sh './docker/dockerize.sh'
+            //         }
+            //     }
+            // }
         }
     }
 }
