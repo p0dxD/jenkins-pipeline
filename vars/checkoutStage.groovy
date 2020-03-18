8b4ae4dea4fb5a65d2721f914a6db7ea78d05225
Jose Rodriguez <jose0797@gmail.com>
2020-03-18 16:23:39 -0400

    Testing

--- vars/checkoutStage.groovy
+++ vars/checkoutStage.groovy
@@ -4,7 +4,6 @@ def call(Config configs) {
     cleanWs()
     checkout scm 
     fillconfiguration(configs)
-    error "Unstable, exiting now..."
     stash "workspace"
 }
 
@@ -16,6 +15,7 @@ private void fillconfiguration(Config configs) {
         String key = entry.getKey();
         ArrayList<String> value = entry.getValue();
         for(String project : value) {
+            //check the diffs if there isnt a change we can just return the pipeline, doesn't need to build.
             String changesCmd = 'if [ '+"${project.path}" + ' != "." ] && [ -z $(git diff HEAD^ HEAD  --name-only | grep '+ "${project.path}" + ') ]; then echo "Empty"; else echo "Has changes."; fi'
            String changesCmdOutput = sh(script: changesCmd, returnStdout: true).trim()
            if (changesCmdOutput.equalsIgnoreCase('Has changes.')) {
@@ -25,4 +25,10 @@ private void fillconfiguration(Config configs) {
         }
     }
     configs.getProjectsConfigs().each{ k, v -> println "${k}:${v.version}" }
+
+    if (configs.getProjectsConfigs().size() == 1) {
+        //we exit pipeline there's no changes, why build? unless triggered by hand. 
+        currentBuild.result = 'SUCCESS'
+        return
+    }
 }
