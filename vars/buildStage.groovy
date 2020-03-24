4ad8b43db02ab06187bc699eb64a18de51ae212a
Jose Rodriguez <jose0797@gmail.com>
2020-03-24 01:41:33 -0400

    Testing

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -32,9 +32,9 @@ def call(PipelineManager pipelineManager){
                             stash name: "${projectPath}${tool}", includes: 'build/**/**'
                         } else if (tool.equals("golang") ) {
                             if ( projectPath.equals("") ) {//we are in a unique situation we move current project into a folder
-                            String commandToExectue = "mkdir -p "+"${env.GOPATH}"+"/project && mv $(pwd)/* "+"${env.GOPATH}"+"/project/"
-                                sh commandToExectue
-                                sh "ls -la "+"${env.GOPATH}"+"/project"
+                            String commandToExectue = "mkdir -p ${env.GOPATH}/project && mv $(pwd)/* ${env.GOPATH}/project/"
+                                echo commandToExectue
+                                // sh "ls -la "+"${env.GOPATH}"+"/project"
                             }
                             // withEnv(["GOPATH=$WORKSPACE", "GOBIN=$GOPATH/bin"]) {
                             //     sh "mkdir src bin && go get ./..."
