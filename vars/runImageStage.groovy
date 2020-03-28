ef306d90adf8cb62d4632a865e13589fd42c3147
Jose Rodriguez <jose0797@gmail.com>
2020-03-27 20:33:14 -0400

    Testing

--- vars/runImageStage.groovy
+++ vars/runImageStage.groovy
@@ -35,11 +35,7 @@ def call(PipelineManager pipelineManager) {
                                      source ~/.bashrc
                                      source ~/.secrets  
                                      docker pull $projectName:latest   
-                                     docker ps -a  
-                                     docker ps -aq --filter name=$name
-                                     export previous_container="\$(docker ps -aq --filter name=$name)"
-                                     echo Previous container: \$previous_container
-                                     if [ -z \$previous_container ]; then echo "No container with that name."; else echo "Cleaning:." && docker stop \$previous_container && docker rm \$previous_container; fi 
+                                     if [ -z "\$(docker ps -aq --filter name=$name)" ]; then echo "No container with that name."; else echo "Cleaning:." && docker stop "\$(docker ps -aq --filter name=$name)" && docker rm "\$(docker ps -aq --filter name=$name)"; fi 
                                      $dockerExecute
                                      "
                                 """
