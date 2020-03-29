ceb51aefff07480ca5d60a16eacfbc66637459c5
Jose Rodriguez <jose0797@gmail.com>
2020-03-29 01:37:33 -0400

    Testing

--- resources/scripts/post-checkout.sh
+++ resources/scripts/post-checkout.sh
@@ -9,12 +9,13 @@
 		echo "Replacing: $variable"
 		value=$(echo "$LINE" | cut -f2 -d" " | cut -f2 -d"="| tr -d '"')
 		echo "Value: $value"
-		exit 1
  		if [ ! -z hide ]; then
-			sed -i -- "s/$variable/$value/g" $FILE_NAME
+			sed -i -- "s/{{$variable}}/$value/g" $FILE_NAME
 		else
-			sed -i -- "s/$value/$variable/g" $FILE_NAME
+			sed -i -- "s/$value/{{$variable}}/g" $FILE_NAME
 		fi
 	done < /home/.secrets
- done < <(find . -not \( -path ./.git -prune \) -type f -follow -print)
+ done < <(find . -not \( -path ./.git -prune \) -type f -follow -print | grep -v "dockerfiles")
+
+ exit 1
  
