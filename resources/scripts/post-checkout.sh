504a5134c602485669001deffcf8f4cbbad3e7f9
Jose Rodriguez <jose0797@gmail.com>
2020-03-29 01:27:51 -0400

    Testing

--- resources/scripts/post-checkout.sh
+++ resources/scripts/post-checkout.sh
@@ -8,6 +8,8 @@
 		variable=$(echo "$LINE" | cut -f2 -d" " | cut -f1 -d"=")
 		echo "Replacing: $variable"
 		value=$(echo "$LINE" | cut -f2 -d" " | cut -f2 -d"="| tr -d '"')
+		echo "Value: $value"
+		exit 1
  		if [ ! -z hide ]; then
 			sed -i -- "s/$variable/$value/g" $FILE_NAME
 		else
