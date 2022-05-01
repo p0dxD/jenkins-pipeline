fcee7444fa42632194054213d7ce896aa3d49133
Jose <jose0797@gmail.com>
2022-05-01 17:06:07 -0400

    Testing

--- vars/checkoutStage.groovy
+++ vars/checkoutStage.groovy
@@ -10,7 +10,7 @@ def call(final PipelineManager pipelineManager) {
 private void fillconfiguration(final PipelineManager pipelineManager) {
     //Read configuration
     final LinkedHashMap datas = readYaml file: 'jenkinsconfig.yaml'
-    print("Data", datas)
+    echo("Data", datas)
     // //Adding project configuration
     // for (Map.Entry<String, ArrayList<String>> entry : datas.entrySet()) {
     //     final String key = entry.getKey();
